260138f6481480c20abf346e1029d66daf2f55a0
Ramin Rabani <41403404+RaminRabani@users.noreply.github.com>
2022-11-23 18:54:44 -0800

    Check for negative quality at the end

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -28,9 +28,7 @@ export class GildedRose {
       }
 
       if (item.name != AGED_BRIE && item.name != BACKSTAGE_PASSES) {
-        if (item.quality > 0) {
-          item.quality--;
-        }
+        item.quality--;
       } else {
         item.quality++;
         if (item.name == BACKSTAGE_PASSES) {
@@ -50,9 +48,7 @@ export class GildedRose {
           item.quality++;
         } else {
           if (item.name != BACKSTAGE_PASSES) {
-            if (item.quality > 0) {
-              item.quality--;
-            }
+            item.quality--;
           } else {
             item.quality = item.quality - item.quality;
           }
@@ -61,6 +57,9 @@ export class GildedRose {
 
       if (item.quality > 50) {
         item.quality = 50;
+      }
+      if (item.quality < 0) {
+        item.quality = 0;
       }
     }
 
