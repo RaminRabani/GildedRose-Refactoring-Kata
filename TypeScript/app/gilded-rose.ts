7d70109733a7cefd6a08c7cba0bb1bccd3c7fb7c
Ramin Rabani <41403404+RaminRabani@users.noreply.github.com>
2022-11-26 11:06:56 -0800

    Add else if to condition

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -80,8 +80,7 @@ export class GildedRose {
   adjustForQualityLimits(item: Item): void {
     if (item.quality > 50) {
       item.quality = 50;
-    }
-    if (item.quality < 0) {
+    } else if (item.quality < 0) {
       item.quality = 0;
     }
   }
